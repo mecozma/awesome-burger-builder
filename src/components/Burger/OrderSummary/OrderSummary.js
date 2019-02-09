cb0f87a48e95d3c334866f182d76af38b12db572
mecozma <mecozma@gmail.com>
2019-02-09 19:18:26 +0000

    Improved the Performance of the app. Converted Modal component to a class component to be able to use shouldComponentUpdate method

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Auxilliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+export default class OrderSummary extends Component {
+  // This component could be a functional component, but I made it Class for debuging matters
+  componentWillUpdate() {
+    console.log('[OrderSummary WillUpdate');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return(
         <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       )
     })
-  return(
-    <Aux>
-      <p>Your burger contains the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Your order price is: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+    return(
+      <Aux>
+        <p>Your burger contains the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Your order price is: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary; 
