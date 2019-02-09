25e8f22cf884c62f74fd085ade0b54558d95a5d5
mecozma <mecozma@gmail.com>
2019-02-09 17:33:20 +0000

    Added a Sidedrawer Toggle Menu Button

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '../../../UI/Button/Button';
+
+const drawerToggle = (props) => (
+  <Button clicked={props.clicked}>MENU</Button>
+);
+
+export default drawerToggle;
