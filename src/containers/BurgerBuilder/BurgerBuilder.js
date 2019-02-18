1c6f17c932e78f225b18b4d3e7a8a864fd0bff34
mecozma <mecozma@gmail.com>
2019-02-18 23:41:26 +0000

    Sent a post request with the order to the firebase database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.3,
@@ -89,7 +90,24 @@ export default class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Your oder has been received');
+    // alert('Your oder has been received');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Mr. Burger',
+        address: {
+          street: 'Cow Lane',
+          zipCode: 6115,
+          country: 'Sauce Land'
+        },
+        email: "hot-wings@bbq.com"
+      },
+      deliveryMethod: 'fast'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render() {
